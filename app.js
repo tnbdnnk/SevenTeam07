f4945f1782d2b384d3102a383c175c937d9fa64b
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-02 00:54:13 +0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import "./db.js";
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/users', authRouter);
+
+app.use((_, res) => {
+    res.status(404).json({ message: "Route Not Found" });
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server Error" } = err;
+    res.status(status).json({ message });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running. Use our API on port: ${PORT}`);
+});
