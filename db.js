1538540a33f6f300b5ccc5a544400ffb28bf8c96
Tatiana Bidnenko <tnbdnnk@gmail.com>
2024-05-02 12:56:51 +0300

    Fix.

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const DB_URI = process.env.DB_URI;
 
 mongoose
-    .connect(DB_URI)
-    .then(() => console.log('Database connection success'))
-    .catch((error) => {
-        console.error('Database connection error:', error);
-        process.exit(1);
-    });
+  .connect(DB_URI)
+  .then(() => console.log("Database connection success"))
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  });
