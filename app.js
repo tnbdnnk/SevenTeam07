c41127bce43ffd4b40765fd88d1a481bae3d1662
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-05-03 15:09:18 +0300

    auth_v2

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import mongoose from "mongoose";
 import "./db.js";
 
 import authRouter from "./routes/authRouter.js";
@@ -13,8 +12,8 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-// app.use('/api/users', authRouter);
 app.use("/api/users", authRouter);
 
 app.use((_, res) => {
@@ -30,3 +29,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running. Use our API on port: ${PORT}`);
 });
+
