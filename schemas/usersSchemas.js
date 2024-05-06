291b81166a2ce0ab655d637d2cb66bfb9810cdd2
IvanSosnin1983 <IvanSosnin1983@gmail.com>
2024-05-07 02:23:48 +0300

    Added update name, pass, email

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,18 +1,26 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-import { emailRegepxp } from "../constants/user-constants.js";
+import { emailRegepxp } from '../constants/user-constants.js';
 
 export const userSignupSchema = Joi.object({
-    name: Joi.string().min(2).max(32).required(),
-    email: Joi.string().pattern(emailRegepxp).required(),
-    password: Joi.string().min(8).max(64).required(),
-})
+  name: Joi.string().min(2).max(32).required(),
+  email: Joi.string().pattern(emailRegepxp).required(),
+  password: Joi.string().min(8).max(64).required(),
+});
 
 export const userSigninSchema = Joi.object({
-    email: Joi.string().pattern(emailRegepxp).required(),
-    password: Joi.string().min(8).max(64).required(),
-})
+  email: Joi.string().pattern(emailRegepxp).required(),
+  password: Joi.string().min(8).max(64).required(),
+});
 
 export const userEmailSchema = Joi.object({
-    email: Joi.string().pattern(emailRegepxp).required(),
-})
+  email: Joi.string().pattern(emailRegepxp).required(),
+});
+
+export const userUpdateSchema = Joi.object({
+  name: Joi.string().min(2).max(32),
+  email: Joi.string().pattern(emailRegepxp).messages({
+    'string.pattern.base': 'Email must be a valid email address',
+  }),
+  password: Joi.string().min(8).max(64),
+});
