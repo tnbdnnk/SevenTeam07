eb2cca6e6814d3c113b9452916e8d1cfadf5f047
Andrii1308 <123685813+Andrii1308@users.noreply.github.com>
2024-05-09 21:35:14 +0300

    upd

--- schemas/columnsSchemas.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const createColumnSchema = Joi.object({
-  title: Joi.string().required(),
-});
-
-export const updateColumnSchema = Joi.object({
-  title: Joi.string(),
-});
