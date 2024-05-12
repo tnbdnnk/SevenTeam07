7f7c35b20f8bde5e43168f3bf3e07feba72789fc
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-05-12 15:44:58 +0300

    auth_v6

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -4,12 +4,20 @@ import { emailRegepxp, themesList } from '../constants/user-constants.js';
 
 export const userSignupSchema = Joi.object({
     name: Joi.string().min(2).max(32).required(),
-    email: Joi.string().pattern(emailRegepxp).required(),
+    email: Joi.string().pattern(emailRegepxp)
+    .messages({
+        'string.pattern.base': 'Email must be a valid email address',
+    })
+    .required(),
     password: Joi.string().min(8).max(64).required(),
 });
 
 export const userSigninSchema = Joi.object({
-    email: Joi.string().pattern(emailRegepxp).required(),
+    email: Joi.string().pattern(emailRegepxp)
+    .messages({
+        'string.pattern.base': 'Email must be a valid email address',
+    })
+    .required(),
     password: Joi.string().min(8).max(64).required(),
 });
 
